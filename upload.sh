d6bb1aeeefc535af39d353aeb3bfbca84b1e6c03
LineInc <43089553+LineInc@users.noreply.github.com>
2019-03-06 09:28:28 +0900

    Create upload.sh

---
+++ upload.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -X PUT -H 'Authorization: Bearer HbwCVIqZbYAAAAAAAAAAD3cMoPlwf45KD4WV8bIo3vgAkr6PEyczElOEMt_Ak6zO' -T ./iPhoneOS12.1.sdk.tar.gz https://content.dropboxapi.com/1/files_put/auto/iPhoneOS12.1.sdk.tar.gz
